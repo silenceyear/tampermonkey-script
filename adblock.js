0260163c35839383cc03614557a7f663a1a70275
yeqi <597019425@qq.com>
2019-12-21 18:56:59 +0800

    easylistchina

--- adblock.js
+++ adblock.js
@@ -4,14 +4,37 @@
 // @version      0.0.1
 // @description  去除所有网站广告
 // @author       silenceyear
-// @require      https://libs.baidu.com/jquery/1.7.2/jquery.min.js
-// @match        http://*
-// @match        https://*
+// @required     https://cdnjs.cloudflare.com/ajax/libs/fuckadblock/3.2.1/fuckadblock.min.js
 // @grant        none
-// @run-at       document-end
 // ==/UserScript==
+function loadjscssfile(filename){
+    var fileref=document.createElement('script')//创建标签
+    fileref.setAttribute("type","text/javascript")//定义属性type的值为text/javascript
+    fileref.setAttribute("src", filename)//文件的地址
+    document.getElementsByTagName("head")[0].appendChild(fileref)
+}
+if (typeof jQuery == 'undefined') {
+    loadjscssfile("https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js");
+}
 $(function () {
-    var url = location.href;
+    var ua = navigator.userAgent;
+    var clientCode = 0b0000;
 
-    console.log(url);
+    if (/MicroMessenger/i.test(ua)) {
+        clientCode = clientCode|0b0001;
+    }
+
+    if (/(iPhone|iPad|iPod|iOS)/i.test(ua)) {
+        clientCode = clientCode|0b0010;
+    } else if (/(Android)/i.test(ua)) {
+        clientCode = clientCode|0b0100;
+    } else {
+        clientCode = clientCode|0b1000;
+    }
+
+    console.log(clientCode);
+
+    if(typeof fuckAdBlock !== 'undefined' || typeof FuckAdBlock !== 'undefined') {
+
+    }
 })();
